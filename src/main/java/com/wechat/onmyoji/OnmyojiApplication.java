1d8eb96889db034147c49441b86acd69813824db
解义斌 <yibin.xie@e-melt.com>
2019-03-06 16:37:33 +0800

    第一次提交

---
+++ src/main/java/com/wechat/onmyoji/OnmyojiApplication.java
@@ -0,0 +1,13 @@
+package com.wechat.onmyoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnmyojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnmyojiApplication.class, args);
+    }
+
+}
